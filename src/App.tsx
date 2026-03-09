92ddaab2c077eefc9841517de5e88f9e16483379
Jynax <jynaxx@gmail.com>
2026-03-09 17:52:15 -0400

    Initial scaffold: React + Vite + TypeScript with dark theme

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="landing">
+      <h1>JynaxxApps</h1>
+      <p className="tagline">Things I'm building.</p>
+    </div>
+  )
+}
+
+export default App
